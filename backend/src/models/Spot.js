a5d6505554478f71465c405581b2e0142a06bc1d
Alcsaw <augustolcschnorr2@gmail.com>
2019-10-05 10:06:28 -0300

    Fix images not showing on Android Device

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -18,7 +18,9 @@ const SpotSchema = new mongoose.Schema({
 
 //Virtual -> field doesn't exists in the database, but it's going to be set via JS.
 SpotSchema.virtual('thumbnail_url').get(function() {
-    return `http://localhost:3333/files/${this.thumbnail}`
+    //return `http://localhost:3333/files/${this.thumbnail}`
+    // Fix images not showing on Android Device
+    return `http://192.168.0.101:3333/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema);
